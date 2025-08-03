abc05cbe7c44bc004ec926f3b13508d4e9bc2a85
nexorqk <pkms2a10@gmail.com>
2025-08-04 01:16:58 +0300

    feat: add dark color for one pokemon

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,9 +1,15 @@
 import { useOutletContext } from 'react-router';
+import { useContext } from 'react';
+
+import { ThemeContext } from '../context/theme';
 import { type MainData } from '../view/main-view';
+import { cn } from '../utils/cn';
 
 export const PokemonCard = () => {
   const { pokemonData: data } =
     useOutletContext<Pick<MainData, 'pokemonData'>>();
+
+  const themeIsDark = useContext(ThemeContext);
 
   if (data === null || data === undefined) return <h2>No data</h2>;
 
@@ -11,7 +17,12 @@ export const PokemonCard = () => {
     const { name, weight, abilities } = data;
 
     return (
-      <div className="p-4 border space-y-4">
+      <div
+        className={cn(
+          'p-4 border space-y-4',
+          themeIsDark ? 'text-white' : 'text-slate-900'
+        )}
+      >
         <h2>Name: {name}</h2>
         <p>Weight: {weight}</p>
         <div>
--- src/components/ui/loader.tsx
+++ src/components/ui/loader.tsx
@@ -1,11 +1,33 @@
+import { useContext } from 'react';
+
+import { ThemeContext } from '../../context/theme';
+import { cn } from '../../utils/cn';
+
 export const Loader = ({ isLoading }: { isLoading?: boolean }) => {
+  const themeIsDark = useContext(ThemeContext);
+
   if (isLoading === false) return null;
 
   return (
     <div role="alert" aria-label="Loading content" className="flex gap-2  p-4 ">
-      <div className="h-4 w-4 bg-black animate-bounce rounded-full [animation-delay:-0.3s]"></div>
-      <div className="h-4 w-4 bg-black animate-bounce rounded-full [animation-delay:-0.15s]"></div>
-      <div className="h-4 w-4 bg-black animate-bounce rounded-full"></div>
+      <div
+        className={cn(
+          'h-4 w-4 bg-black animate-bounce rounded-full [animation-delay:-0.3s]',
+          themeIsDark ? 'bg-white' : 'bg-slate-900'
+        )}
+      ></div>
+      <div
+        className={cn(
+          'h-4 w-4 animate-bounce rounded-full [animation-delay:-0.15s]',
+          themeIsDark ? 'bg-white' : 'bg-slate-900'
+        )}
+      ></div>
+      <div
+        className={cn(
+          'h-4 w-4 bg-black animate-bounce rounded-full',
+          themeIsDark ? 'bg-white' : 'bg-slate-900'
+        )}
+      ></div>
       <div className="sr-only" aria-label="Loading content">
         Loading content
       </div>
