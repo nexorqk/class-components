9a41d31b708704cedc272c675f35a1ff4b4abc24
nexorqk <pkms2a10@gmail.com>
2025-08-23 15:27:10 +0300

    chore: add libs

---
+++ src/App.test.tsx
@@ -0,0 +1,13 @@
+import { describe, expect, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/vitest';
+
+import App from './App';
+
+describe('App', () => {
+  it('Test for aprp mount', () => {
+    render(<App />);
+
+    expect(screen.getByRole('heading')).toHaveTextContent(/react/i);
+  });
+});
